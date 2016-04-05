430c487e753af5aafbc37dab46e235994237c0b0
Nick Berejan <njberejan@gmail.com>
2016-04-05 13:47:52 -0400

    first commit

---
+++ GameOfSticks.py
@@ -0,0 +1,25 @@
+# Decide on the data structure(s) that you are going to need to use to represent the state of the game when played.
+# Decide on the tasks that will be part of the game loop.
+# Write test function(s) for a single task.
+# Write function(s) for the task and make sure your test(s) pass.
+# Repeat steps 3 and 4 until all tasks are implemented.
+# Write a main() function with a game loop that uses your already tested and developed functionality in conjunction with getting user input and printing output.
+
+class Sticks:
+    def __init__(self):
+        self.number_of_sticks = number_of_sticks
+
+    def ___subtract___(self):
+        return self.number_of_sticks = number_of_sticks - number
+
+    def game_over(self):
+        if self.number_of_sticks < 1:
+
+def main():
+    While True:
+        print("Welcome to Game of Sticks!")
+        print("There are {} sticks remaining.".format(Sticks.number_of_sticks))
+    #declares there are 20 sticks
+    #prompt user to choose a number of sticks between 1-3
+    #returns number of sticks
+    #checks for game over condition
