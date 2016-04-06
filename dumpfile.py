38163cd6c6ee24f4d47c8b3d0d8ba733ee5496c6
Nick Berejan <njberejan@gmail.com>
2016-04-05 21:27:40 -0400

    implemented vs. computer mode

--- dumpfile.py
+++ dumpfile.py
@@ -7,12 +7,39 @@
 
 from sticks import Sticks
 from human_v_human import Human_v_Human
-
+from human_v_cpu import Human_v_CPU
+cpu_dict = {
+0: [1, 2, 3],
+1: [1, 2 ,3],
+2: [1, 2, 3],
+3: [1, 2, 3],
+4: [1, 2, 3],
+5: [1, 2, 3],
+6: [1, 2, 3],
+7: [1, 2, 3],
+8: [1, 2, 3],
+9: [1, 2, 3],
+10: [1, 2, 3],
+11: [1, 2, 3],
+12: [1, 2, 3],
+13: [1, 2, 3],
+14: [1, 2, 3],
+15: [1, 2, 3],
+16: [1, 2, 3],
+17: [1, 2, 3],
+18: [1, 2, 3],
+19: [1, 2, 3],
+20: [1, 2, 3],
+}
 def human_or_ai():
     return input("Press 'h' to play versus a human, or 'c' to play versus the computer: ").lower()
 
 
 def main():
+    # cpu_dict = {0: [1, 2, 3], 1: [1, 2 ,3], 2: [1, 2, 3], 3: [1, 2, 3], 4: [1, 2, 3]. 5: [1, 2, 3], 6: [1, 2, 3]}
+        # 7:[1,2,3], 8:[1,2,3], 9:[1,2,3], 10:[1,2,3], 11:[1,2,3], 12:[1,2,3], 13:[1,2,3],
+        # 14:[1,2,3], 15:[1,2,3], 16:[1,2,3], 17:[1,2,3], 18:[1,2,3], 19:[1,2,3], 20:[1,2,3]
+    #}
     sticks = Sticks(20)
     turn_count = 0
     sticks_left = sticks.number_of_sticks
@@ -22,7 +49,7 @@ def main():
     if game_mode == 'h':
         Human_v_Human.game(sticks_left, turn_count, sticks)
     elif game_mode == 'c':
-        pass
+        Human_v_CPU.game(sticks_left, turn_count, sticks, cpu_dict)
 
     #declares there are 20 sticks
     #prompt user to choose a number of sticks between 1-3
---
+++ human_v_cpu.py
@@ -0,0 +1,51 @@
+import random
+from sticks import Sticks
+
+
+class Human_v_CPU():
+
+
+    def __init__(self, players):
+        self.players = players
+
+    def cpu_turn(turn_count, cpu_dict):
+        for turn_count in cpu_dict:
+            return cpu_dict[random.randint(1, 3)]
+
+    def game(sticks_left, turn_count, sticks, cpu_dict):
+        while True:
+            if sticks.game_over(sticks_left):
+                print("The game is over.")
+                break
+            elif sticks_left == 1:
+                if turn_count % 2 == 0:
+                    print("You have no choice but to take the remaining stick. You have lost.")
+                    break
+                else:
+                    print("The computer has no choice but to take the remaining stick. You win!")
+                    break
+            elif turn_count % 2 == 0:
+                print("There are {} sticks remaining.".format(sticks_left))
+                player_one_choice = sticks.player_one_turn()
+                sticks_left = sticks_left - player_one_choice
+                print(sticks_left)
+                turn_count += 1
+                continue
+            elif turn_count % 2 == 1:
+                print("There are {} sticks remaining.".format(sticks_left))
+                cpu_choice_list = Human_v_CPU.cpu_turn(turn_count, cpu_dict)
+                print(cpu_choice_list)
+                cpu_choice = random.choice(cpu_choice_list)
+                print(cpu_choice)
+                print("The CPU picks up {} sticks.".format(cpu_choice))
+                sticks_left = sticks_left - cpu_choice
+                print(sticks_left)
+                turn_count += 1
+                continue
+            else:
+                print("There are {} sticks remaining.".format(sticks_left))
+                player_one_choice = sticks.player_one_turn()
+                sticks_left = sticks_left - player_one_choice
+                print(sticks_left)
+                turn_count += 1
+                continue
